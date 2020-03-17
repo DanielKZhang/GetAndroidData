304f213780f50282a128c374fd01d05963a5975f
DanielKZhang <15022199965@163.com>
2020-03-17 19:37:06 +0800

    20200317

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 
 //接收端
+//记录数据0.2s
 public class Main {
 
 
@@ -36,33 +39,33 @@ public class Main {
 
     public static  void main(String argus[])
     {
+        File datafile = new File("a.txt");
         try {
-        server_Socket = new ServerSocket(12345);
-        while(true) {
+            server_Socket = new ServerSocket(12345);
+//        while(true) {
             socekt = server_Socket.accept();
-//            InputStream is = socekt.getInputStream();
-//            InputStreamReader isr = new InputStreamReader(is);
-//            BufferedReader bf = new BufferedReader(isr);
-//            String demand;
-
-//            while ( (demand = bf.readLine()  )!= null) {
-//                System.out.println(demand);
-//
-//            }
-
              //获取二进制流
             DataInputStream input=new DataInputStream(socekt.getInputStream());
+
+           //文件设置
+            FileWriter out = new FileWriter(datafile);
+            String line = System.getProperty("line.separator");
+
+
             byte[] buffer = new byte[20480];
             //消息长度
             int rlength=input.read(buffer, 0, 20480);
             while(rlength != -1) {
-                System.out.println("接收的消息长度:" + rlength);
+//                System.out.println("接收的消息长度:" + rlength);
                 int f = bytesToInt(buffer, 0);
                 System.out.println("接收的消息:" + f);
                 rlength=input.read(buffer, 0, 20480);
+                out.write("" + f);;
+                out.write(line);
             }
+            out.close();
 
-        }
+//        }
     } catch (IOException e) {
         e.printStackTrace();
     }
